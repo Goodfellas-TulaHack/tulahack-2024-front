5225c119537fc78d0c1b70c0155c2838151b2b22
Xtron33 <xtron_uk@yahoo.com>
2024-04-27 00:18:44 +0300

    add restraunt

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,13 @@
 import {useState} from "react";
-import {Button, Center, Divider, Input, PasswordInput, Stack, Text} from "@mantine/core";
+import {Button, Center, Divider, Input, MultiSelect, PasswordInput, Stack, Tabs, Text} from "@mantine/core";
 import {IMaskInput} from "react-imask";
+import {IconHome, IconUser} from "@tabler/icons-react";
 
 
 const Login = () => {
     const [isRegister, setRegister] = useState<boolean>(false);
+
+    const typeOfKitchen = ["Вьетнамская", "Мексиканская","Паназиатская"]
 
     const [login, setLogin] = useState<string>('');
     const [password, setPassword] = useState<string>('');
@@ -12,6 +15,11 @@ const Login = () => {
     const [lastName, setLastName] = useState<string>('');
     const [middleName, setMiddleName] = useState<string>('');
     const [phone, setPhone] = useState<string>('');
+    const [title, setTitle] = useState<string>('');
+    const [address, setAddress] = useState<string>('');
+    const [type, setType] = useState<string[]>([]);
+
+    const [tab, setTab] = useState<string>('Пользователь');
 
     return(
         <Center h={"100dvh"}>
@@ -24,10 +32,22 @@ const Login = () => {
                         Гастрохаб
                     </Text>
                     <Divider size={"sm"}/>
+                    {isRegister &&
+                        <Tabs onChange={(e) => setTab(e ? e : '')} value={tab} defaultValue="Пользователь">
+                            <Tabs.List grow>
+                                <Tabs.Tab value="Пользователь" leftSection={<IconUser />}>
+                                    Пользователь
+                                </Tabs.Tab>
+                                <Tabs.Tab value="Ресторан" leftSection={<IconHome/>}>
+                                    Ресторан
+                                </Tabs.Tab>
+                            </Tabs.List>
+                        </Tabs>
+                    }
 
                     <Input required minLength={4} variant="filled" placeholder="Логин "/>
                     {
-                        isRegister &&
+                        isRegister && tab === 'Пользователь' &&
                         <>
 
                             <Input required minLength={1} variant="filled" placeholder="Имя"/>
@@ -36,6 +56,20 @@ const Login = () => {
                             <Input required component={IMaskInput} mask="+7 (000) 000-00-00" type={"phone"}
                                    variant="filled" placeholder="Номер телефона"/>
 
+                        </>
+                    }
+                    {
+                        isRegister && tab === 'Ресторан' &&
+                        <>
+                            <Input required minLength={4} variant="filled" placeholder="Название ресторана "/>
+                            <Input required minLength={4} variant="filled" placeholder="Адрес "/>
+                            <MultiSelect
+                                value={type}
+                                onChange={(e) => setType(e)}
+                                variant="filled"
+                                placeholder="Тип кухни"
+                                data={typeOfKitchen}
+                            />
                         </>
                     }
                     <PasswordInput required minLength={6} variant="filled" type={"password"} placeholder="Пароль"/>
