83c8ba006e87503bc557e55ad111ab480ea28ae5
Askent7 <vie2001@yandex.ru>
2024-04-27 10:38:55 +0300

    add queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import "@mantine/core/styles.css";
 import { createTheme, MantineProvider } from "@mantine/core";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.tsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
 function App() {
   const theme = createTheme({});
+  const queryClient = new QueryClient();
 
   return (
-    <MantineProvider theme={theme}>
-      <RouterProvider router={router} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
 
