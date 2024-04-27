d9b3eec1c22dd2f65f800759914a5112328e41d1
Askent7 <vie2001@yandex.ru>
2024-04-27 09:21:47 +0300

    относительные пути

--- src/components/media.tsx
+++ src/components/media.tsx
@@ -2,5 +2,5 @@ import { useMediaQuery } from "@mantine/hooks";
 
 export function useMobileQuery() {
   const mobile = useMediaQuery("(max-width: 25rem)");
-  return { mobile };
+  return mobile;
 }
