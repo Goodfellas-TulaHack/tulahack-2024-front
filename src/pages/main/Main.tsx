f731e03b974b0e9cc67295822b233c2c8db6c308
Askent7 <vie2001@yandex.ru>
2024-04-28 12:24:31 +0300

    fix filter

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -16,13 +16,19 @@ import { useGetListKitchen } from "@/api/kitchen/kitchen.api";
 
 const Main = () => {
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+  const { data: kitchenResp } = useGetListKitchen();
 
   const { data: restoran, isLoading } = useGetListRestoran({
     title: search,
-    kitchenIds: [],
+    //@ts-ignore
+    kitchenIds:
+      kitchenResp && filter !== ""
+        ? [kitchenResp?.find((elem) => elem.name == filter)?.id]
+        : [],
   });
 
-  const { data: kitchenResp } = useGetListKitchen();
+  console.log(kitchenResp?.find((elem) => elem.name == filter)?.id);
 
   return (
     <Flex direction="column" gap="md">
@@ -50,6 +56,7 @@ const Main = () => {
       />
 
       <Select
+        onChange={(value) => setFilter(value ?? "")}
         px={10}
         size="md"
         placeholder="Выберите кухню"
